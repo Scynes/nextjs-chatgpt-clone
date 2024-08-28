e2347a2544dd3dacff15262ca9c334c690e456e0
Scynes <dustin.greyfield@gmail.com>
2024-08-28 13:30:29 -0400

    refactor: removed examples from page and created examples.ts to hold the constant values for use.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,6 @@
 import { ChatExampleButton } from '@/components/core/chat/chat-example-button';
 import { MessageInput } from '@/components/core/chat/message-input';
 import { Box, Container, Flex, Grid, Text } from '@radix-ui/themes';
-import { IoSchoolOutline, IoListOutline } from "react-icons/io5";
-import { HiOutlineLightBulb } from "react-icons/hi";
-import { GoPencil } from "react-icons/go";
 import { useEffect, useRef, useState } from 'react';
 import { submitMessage } from '@/actions/submit-message';
 import { CoreMessage } from 'ai';
@@ -13,6 +10,7 @@ import { readStreamableValue } from 'ai/rsc';
 
 import Image from 'next/image';
 import { ScrollingFlexContainer } from '@/components/shared/scrolling-flex-container';
+import { EXAMPLES } from '@/constants/examples';
 
 export default function Page() {
 
@@ -34,13 +32,6 @@ export default function Page() {
         }
     }
 
-    const EXAMPLES = [
-        { title: <IoSchoolOutline size={ '1.5rem' } className={ 'text-[--indigo-9]' } />, description: 'Teach me about Next.JS.' },
-        { title: <IoListOutline size={ '1.5rem' } className={ 'text-[--tomato-9]' } />, description: 'Make me a recipe with chicken and pesto.' },
-        { title: <HiOutlineLightBulb size={ '1.5rem' } className={ 'text-[--orange-9]' } />, description: 'Fun fact about TypeScript.' },
-        { title: <GoPencil size={ '1.5rem' } className={ 'text-[--jade-9]' } />, description: 'Write a short story about fluffy cats.' },
-    ];
-
     return (
         <Grid rows={ '1fr auto' } className={ 'h-full px-3 pb-5' }>
             <Container maxWidth={ '770px' } asChild>
@@ -52,7 +43,7 @@ export default function Page() {
                                 <Box key={ index } onClick={ () => handleMessageSubmit(example.description) }>
                                     <ChatExampleButton>
                                         <Box className={ 'mx-auto' }>
-                                            { example.title }
+                                            <example.icon size={ '1.5rem' } className={ `text-[${ example.color }]` } />
                                         </Box>
                                         <Text size={ '1' } className={ 'text-[--gray-9]' }>
                                             { example.description }
---
+++ src/constants/examples.ts
@@ -0,0 +1,26 @@
+import { IoSchoolOutline, IoListOutline } from "react-icons/io5";
+import { HiOutlineLightBulb } from "react-icons/hi";
+import { GoPencil } from "react-icons/go";
+
+export const EXAMPLES = [
+    {
+        icon: IoSchoolOutline,
+        color: '--indigo-9',
+        description: 'Teach me about Next.JS.' 
+    },
+    { 
+        icon: IoListOutline,
+        color: '--orange-9',
+        description: 'Make me a recipe with chicken and pesto.'
+    },
+    {
+        icon: HiOutlineLightBulb,
+        color: '--tomato-9',
+        description: 'Fun fact about TypeScript.'
+    },
+    {
+        icon: GoPencil, 
+        color: '--jade-9',
+        description: 'Write a short story about fluffy cats.'
+    },
+];
